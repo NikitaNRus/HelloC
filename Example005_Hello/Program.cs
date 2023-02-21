1219923723c4654c242c35b885275251c1aa9545
NikitaNRus <nikitanrus@mail.ru>
2023-02-21 22:26:15 +0300

    Added project 005

---
+++ Example005_Hello/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "никита")
+{
+    Console.WriteLine("Ура, это же Никита!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
