5a8516d68639b0367da1cad8153d33cec419d250
NikitaNRus <nikitanrus@mail.ru>
2023-02-21 22:15:36 +0300

    Corrected

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,5 +1,5 @@
-﻿double numberA = new Random().Next(1,10); //1 2 3 4 5... max= 9
+﻿double numberA = new Random().Next(1,10); //1 2 3 4 5... max= 9//
 double numberB = new Random().Next(1,10);
-Console.Write(numberA);
-Console.Write(numberB);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
