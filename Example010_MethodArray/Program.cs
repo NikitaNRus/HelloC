f40c1d051c09675b8b17b695860c70e9eceabc6a
NikitaNRus <nikitanrus@mail.ru>
2023-02-25 14:21:41 +0300

    New example 010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {12,23,3,4,5,6,7,8,9,10,11,12,15,65};
+int n = array.Length;
+int find = 12;
+
+int index =0;
+
+while (index <n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1; // Или index++ 
+}
