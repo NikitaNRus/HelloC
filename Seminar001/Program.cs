01a5da9a85ac29dc41416f2a13709a8220273ae3
NikitaNRus <nikitanrus@mail.ru>
2023-02-25 16:02:31 +0300

    Начали семинар 1

---
+++ Seminar001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа принимает на вход целое число и возводит его в квадрат
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num*num;
+Console.WriteLine("Квадрат числа равен: "+result);
+
+//Проверка, что число равно определенному значению
+if (num==5)
+{
+    Console.WriteLine("Пятерочка!");
+}
+else if(num==3)
+{
+    Console.WriteLine("Троечка!");
+}
+else
+{
+    Console.WriteLine("Не троечка и не пятерочка");
+}
