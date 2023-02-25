5083b8ac914d0855c8502764c20a5c916166bbe7
NikitaNRus <nikitanrus@mail.ru>
2023-02-25 19:23:23 +0300

    Added HW1-T2

---
+++ HomeWork1-T2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+int NumA = Convert.ToInt32(Console.ReadLine());
+int NumB = Convert.ToInt32(Console.ReadLine());
+int max = NumA;
+int min = NumB;
+if (NumA<NumB)
+{
+   max = NumB;
+   min = NumA;
+}
+Console.WriteLine("Максимальное число: "+ max);
+Console.WriteLine("Минимальное число: "+ min);
