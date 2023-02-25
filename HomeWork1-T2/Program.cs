3fe66dc2136a16392ede5ab52a694871eade286e
NikitaNRus <nikitanrus@mail.ru>
2023-02-25 19:34:55 +0300

    Corrected

--- HomeWork1-T2/Program.cs
+++ HomeWork1-T2/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
-
+Console.WriteLine("Введите два числа ");
 int NumA = Convert.ToInt32(Console.ReadLine());
 int NumB = Convert.ToInt32(Console.ReadLine());
 int max = NumA;
