ecaf154dc3dfa182916f812ce384a4e93b43bef5
NikitaNRus <nikitanrus@mail.ru>
2023-02-25 19:56:13 +0300

    Added HW1-8

---
+++ HomeWork1-T8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите целое число ");
+int NumA = Convert.ToInt32(Console.ReadLine());
+int start =1;
+while(start<=(NumA))
+{
+    if((start%2==0))
+    {Console.Write(start+" ");
+    start++;
+    }
+    else
+    start++;
+}
