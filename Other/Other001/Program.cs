540d40ce02f9f3b8f7f6b19ab2ca070f7c747fa4
NikitaNRus <nikitanrus@mail.ru>
2023-03-04 12:52:20 +0300

    Добавили Example012

--- Other/Other001/Program.cs
+++ Other/Other001/Program.cs
@@ -1,28 +1,40 @@
 ﻿// Зададим кол-во периодов
-Console.WriteLine("Сколько периодов используется, включая момент инвестиций");
-var n = Convert.ToInt32(Console.ReadLine());
-// Зададим инвестиции в t0
-Console.WriteLine("Сколько инвестиций осуществлено");
-var inv = Convert.ToInt32(Console.ReadLine());
-// Определим кол-во нодов для n-ого периода
-var nodes = Math.Pow(2,n-1);
+// Console.WriteLine("Сколько периодов используется, включая момент инвестиций");
+// var n = Convert.ToInt32(Console.ReadLine());
+// // Зададим инвестиции в t0
+// Console.WriteLine("Сколько инвестиций осуществлено");
+// var inv = Convert.ToInt32(Console.ReadLine());
+// // Определим кол-во нодов для n-ого периода
+// var nodes = Math.Pow(2,n-1);
 
-// Внесли в массив инвестиции
-    double [,] mass = new double [n, (int)nodes];
-    int rows = mass.GetUpperBound(0) + 1;    // количество строк
-    int columns = mass.Length / rows;        // количество столбцов
-    for (int i = 0; i < columns; i++)
-    {
-        mass[0,i]=-inv;
-    }
-    for (int i = 0; i < columns; i++)
+// // Внесли в массив инвестиции
+//     double [,] mass = new double [n, (int)nodes];
+//     int rows = mass.GetUpperBound(0) + 1;    // количество строк
+//     int columns = mass.Length / rows;        // количество столбцов
+//     for (int i = 0; i < columns; i++)
+//     {
+//         mass[0,i]=-inv;
+//     }
+//     for (int i = 0; i < columns; i++)
 
 
 //просмотр массива
+// {
+//     for (int j = 0; j < rows; j++)
+//     {
+//         Console.Write($"{mass[j, i]} \t");
+//     }
+//     Console.WriteLine();
+// }
+
+
+var i = 0.9;
+int start = 0;
+double lim =-1000;
+while (start < 100)
 {
-    for (int j = 0; j < rows; j++)
-    {
-        Console.Write($"{mass[j, i]} \t");
-    }
-    Console.WriteLine();
+    lim = lim+1000*i;
+    i=i*i;
+    start++;
 }
+Console.WriteLine(lim);
