6b61d7e9bb6d2dfbd799a84225c03daaac0bb3ec
NikitaNRus <nikitanrus@mail.ru>
2023-03-07 21:55:06 +0300

    Added HW3-21 HARD

---
+++ HomeWorks/HomeWork3-T21/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Введите мерность пространства");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте координаты первой точки");
+int [] coordA = new int[N];
+for (int i =0; i<N;i++)
+{
+ Console.WriteLine($"Введите {i+1} координату числа");
+ coordA[i]=Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Задайте координаты второй точки");
+int [] coordB = new int[N];
+for (int i =0; i<N;i++)
+{
+ Console.WriteLine($"Введите {i+1} координату числа");
+ coordB[i]=Convert.ToInt32(Console.ReadLine());
+}
+// Просмотр ваших массивов
+Console.WriteLine("координаты первой точки:");
+foreach (int i in coordA)
+{
+    System.Console.Write("{0} ", i);
+}
+Console.WriteLine(" ");
+Console.WriteLine("координаты второй точки:");
+foreach (int i in coordB)
+{
+    System.Console.Write("{0} ", i);
+}
+// Евклидово расстояние равно корень суммы квадратов разностей координат точек
+
+    void EvclDist (int N, int []coordA, int []coordB)
+    {
+    double res = 0;
+    double distance = 0;
+    for(int i=0;i < N;i++)
+       {
+       res =res + Math.Pow(coordA[i]+coordB[i],2);
+       }
+    distance = Math.Sqrt(Convert.ToDouble(res));
+    Console.WriteLine($"Евклидово расстояние между вашими точками в {N}-мерном пространстве равно:");
+    Console.WriteLine(distance);
+    }
+EvclDist(N, coordA,coordB);
