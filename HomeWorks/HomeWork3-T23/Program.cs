a41a794a4ee231aa310e5cdbc6214c57733c9d4f
NikitaNRus <nikitanrus@mail.ru>
2023-03-07 22:00:41 +0300

    Added HW3-23

---
+++ HomeWorks/HomeWork3-T23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+void power3 (int N)
+{
+    for(int i = 1; i<=N;i++)
+    {
+        int power = i*i*i;
+        Console.WriteLine(i+"->"+power);
+    }
+}
+
+power3(N);
