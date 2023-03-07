14a8d11bdee491ead3e2b8186911e04490ddb3f8
NikitaNRus <nikitanrus@mail.ru>
2023-03-07 20:49:42 +0300

    Added HW3-19 HARD

---
+++ HomeWorks/HomeWork3-T19/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+// Через строку решать нельзя.
+
+
+int reverse (int x, int rev)
+{
+    while (x >0)
+    {
+        int dig = x%10;
+        rev = rev*10+dig;
+        x = x/10;
+    }
+    return rev;
+}
+void pallindrom (int x, int y)
+{
+    if (x==y)
+    Console.WriteLine("Это паллиндром");
+    else
+    {
+    Console.WriteLine("Это не паллиндром");
+    }
+}
+
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int rev = 0;
+pallindrom(reverse(num, rev), temp);
