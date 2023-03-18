a27732ba6aa87101b3a5dd311b60a07e6d7dc639
NikitaNRus <nikitanrus@mail.ru>
2023-03-18 15:21:55 +0300

    added HW6

---
+++ HomeWorks/HomeWork6-T41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите размерность массива");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [M];
+
+int [] FillArray(int [] array)
+{
+    for(int i =0;i<array.Length;i++)
+    {
+        Console.WriteLine($"Введите {i+1} число массива");
+        array[i]=Convert.ToInt32(Console.ReadLine());
+    }
+return array;
+}
+
+int Count (int[] array)
+{   int count=0;
+    for(int i =0; i<array.Length; i++)
+    {
+        if (array[i]>0)
+        count ++;
+    }
+    return count;
+}
+
+int sum = Count(FillArray(array));
+Console.WriteLine($"Количество элементов больше нуля = {sum}");
---
+++ HomeWorks/HomeWork6-T43/Program.cs
@@ -0,0 +1,40 @@
+﻿// напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//  значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите константу для первого уравнения");
+int a1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите коэффициент наклона для первого уравнения");
+int b1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите константу для второго уравнения");
+int a2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите коэффициент наклона для второго уравнения");
+int b2=Convert.ToInt32(Console.ReadLine());
+
+bool CheckIntercept(int a1, int b1, int a2, int b2)
+{   
+    if (b1==b2) return true;
+    else return false;
+}
+
+void PrintArray(int[]array)
+{
+    foreach(int i in array)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
+
+void Interception (int a1, int b1, int a2, int b2)
+{
+    if (CheckIntercept(a1,b1,a2,b2)==false)
+    {
+    int X = (a1-a2)/(b1-b2);
+    int Y = a1 +b1*X;
+    int [] array = {X,Y};
+    Console.WriteLine("Координаты точки пересечения:");
+    PrintArray(array);
+    }
+    else Console.WriteLine("Линии параллельны, либо овпадают");
+}
+Interception(a1,b1,a2,b2);
